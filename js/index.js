21fc14166d64112f9b5c78277c480071e1e220d7
Marcos Saenz <66282209+Marcos-s-c@users.noreply.github.com>
2023-06-25 23:03:21 -0600

    game over finish

--- js/index.js
+++ js/index.js
@@ -56,17 +56,39 @@ function spawnEnemies(spawnCount) {
 const buildings = [];
 let activeTile = undefined;
 let enemyCount = 3;
+let hearts = 10;
 
 spawnEnemies(enemyCount);
 
 //recursive function
 function animate() {
-  requestAnimationFrame(animate);
+  const animationId = requestAnimationFrame(animate);
+
   c.drawImage(image, 0, 0);
 
-  enemies.forEach((enemy) => {
+  for (let i = enemies.length - 1; i >= 0; i--) {
+    const enemy = enemies[i];
     enemy.update();
-  });
+
+    if (enemy.position.x > canvas.width) {
+      console.log("enemy exit the map");
+      hearts -= 1;
+      enemies.splice(i, 1);
+      console.log(hearts);
+
+      if (hearts === 0) {
+        console.log("game over");
+        cancelAnimationFrame(animationId);
+        document.querySelector("#gameOver").style.display = "flex";
+      }
+    }
+  }
+
+  //tracking total amount of enemies
+  if (enemies.length === 0) {
+    enemyCount += 2;
+    spawnEnemies(enemyCount);
+  }
 
   placementTiles.forEach((tile) => {
     tile.update(mouse);
@@ -108,11 +130,6 @@ function animate() {
           if (enemyIndex > -1) enemies.splice(enemyIndex, 1);
         }
 
-        //tracking total amount of enemies
-        if (enemies.length === 0) {
-          enemyCount += 2;
-          spawnEnemies(enemyCount);
-        }
         console.log(projectile.enemy.health);
         building.projectiles.splice(i, 1);
       }
